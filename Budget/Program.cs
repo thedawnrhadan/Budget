61abb1cacafacb77a20051cfaa56338ee9ec2032
Romel Fraatz <fraatz.rj@gmail.com>
2023-01-21 18:33:03 -0700

    Adding new features

--- Budget/Program.cs
+++ Budget/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Budget
@@ -8,6 +9,8 @@ namespace Budget
         double income, expenses, difference;
         bool saveData;
         string dataFile = "budget.txt";
+        Dictionary<string, double> incomeCategories = new Dictionary<string, double>();
+        Dictionary<string, double> expenseCategories = new Dictionary<string, double>();
 
         // A constructor that initializes variables
         public Budget()
@@ -21,10 +24,31 @@ namespace Budget
         // A method to get income and expenses data from user
         public void GetBudgetData()
         {
-            Console.Write("Enter income: ");
-            this.income = double.Parse(Console.ReadLine());
-            Console.Write("Enter expenses: ");
-            this.expenses = double.Parse(Console.ReadLine());
+            // Get income data
+            Console.Write("Enter number of income categories: ");
+            int numIncomeCategories = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numIncomeCategories; i++)
+            {
+                Console.Write("Enter name of income category {0}: ", i+1);
+                string incomeCategory = Console.ReadLine();
+                Console.Write("Enter amount for {0}: ", incomeCategory);
+                double incomeAmount = double.Parse(Console.ReadLine());
+                incomeCategories.Add(incomeCategory, incomeAmount);
+                this.income += incomeAmount;
+            }
+
+            // Get expenses data
+            Console.Write("Enter number of expenses categories: ");
+            int numExpenseCategories = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numExpenseCategories; i++)
+            {
+                Console.Write("Enter name of expense category {0}: ", i+1);
+                string expenseCategory = Console.ReadLine();
+                Console.Write("Enter amount for {0}: ", expenseCategory);
+                double expenseAmount = double.Parse(Console.ReadLine());
+                expenseCategories.Add(expenseCategory, expenseAmount);
+                this.expenses += expenseAmount;
+            }
         }
 
         // A method to calculate the budget difference
@@ -50,7 +74,7 @@ namespace Budget
             do
             {
                 Console.Write("Do you want to save the data? (yes/no): ");
-                string save = Console.ReadLine();
+                string save = Console.ReadLine().ToLower();
                 if (save == "yes")
                 {
                     this.saveData = true;
@@ -59,7 +83,6 @@ namespace Budget
                 else if (save == "no")
                 {
                     this.saveData = false;
-                    break;
                 }
                 else
                 {
@@ -70,21 +93,93 @@ namespace Budget
             // If user wants to save data, write data to the budget.txt file
             if (this.saveData)
             {
+                if (File.Exists(dataFile))
+                {
+                    File.Delete(dataFile);
+                }
                 using (StreamWriter sw = new StreamWriter(dataFile))
                 {
-                    sw.WriteLine("Income: " + this.income);
-                    sw.WriteLine("Expenses: " + this.expenses);
-                    sw.WriteLine("Difference: " + this.difference);
+                    sw.WriteLine("Income Categories:");
+                    foreach (var income in incomeCategories)
+                    {
+                        sw.WriteLine("{0}: {1}", income.Key, income.Value);
+                    }
+                    sw.WriteLine("Total Income: {0}", this.income);
+                    sw.WriteLine("Expense Categories:");
+                    foreach (var expense in expenseCategories)
+                    {
+                        sw.WriteLine("{0}: {1}", expense.Key, expense.Value);
+                    }
+                    sw.WriteLine("Total Expenses: {0}", this.expenses);
+                    sw.WriteLine("Difference: {0}", this.difference);
                 }
             }
         }
         
+        // A method to read the data from the budget.txt file
+        public void LoadData()
+        {
+            if (File.Exists(dataFile))
+            {
+                using (StreamReader sr = new StreamReader(dataFile))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+        }
+        
+        // A method to display the budget data
+        public void DisplayBudget()
+        {
+            Console.WriteLine("Income Categories:");
+            foreach (var income in incomeCategories)
+            {
+                Console.WriteLine("{0}: {1}", income.Key, income.Value);
+            }
+            Console.WriteLine("Total Income: {0}", this.income);
+            Console.WriteLine("Expense Categories:");
+            foreach (var expense in expenseCategories)
+            {
+                Console.WriteLine("{0}: {1}", expense.Key, expense.Value);
+            }
+            Console.WriteLine("Total Expenses: {0}", this.expenses);
+            CalculateBudget();
+        }
+        // Main Method
         static void Main(string[] args)
         {
-            Budget budget = new Budget();
-            budget.GetBudgetData();
-            budget.CalculateBudget();
-            budget.SaveData();
+            try
+            {
+                Budget budget = new Budget();
+                Console.Write("Do you want to load data? (yes/no): ");
+                string load = Console.ReadLine().ToLower();
+                if (load == "yes")
+                {
+                    budget.LoadData();
+                }
+                else if (load == "no")
+                {
+                    budget.GetBudgetData();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter yes or no.");
+                }
+                budget.DisplayBudget();
+                budget.SaveData();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,27 @@
+# Overview
+
+This program allows users to track their income and expenses. This would help users to insert their income and expenses into the application to see a summary of their current financial situation.
+
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+
+{Describe the programming language that you used and any libraries.}
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- [Web Site Name](http://url.link.goes.here)
+- [Web Site Name](http://url.link.goes.here)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+
+- Item 1
+- Item 2
+- Item 3
